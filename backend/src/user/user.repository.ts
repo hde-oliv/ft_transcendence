886901362cfbaf185cfe90495b15cc2494d72816
Henrique de Campos Duller <henriqueduller@hotmail.com>
2023-10-05 18:57:18 -0300

    changed user.repository , since model User has changed to Users, additional changes are needed in order for it to work

--- backend/src/user/user.repository.ts
+++ backend/src/user/user.repository.ts
@@ -9,24 +9,24 @@ export class UserRepository {
   constructor(private readonly prismaService: PrismaService) { }
 
   async getUserById(id: number): Promise<UserEntity | null> {
-    return this.prismaService.user.findUnique({ where: { id: Number(id) } });
+    return this.prismaService.users.findUnique({ where: { id: Number(id) } });
   }
 
   async getUserByIntra(intra_tag: string): Promise<UserEntity | null> {
-    return this.prismaService.user.findUnique({ where: { intraLogin: intra_tag } })
+    return this.prismaService.users.findUnique({ where: { intra_login: intra_tag } })
   }
 
   async getAllUsers(): Promise<Array<UserEntity | null>> {
-    return this.prismaService.user.findMany();
+    return this.prismaService.users.findMany();
   }
 
   async createRandomUser(): Promise<UserEntity> {
     const randomUser = createRandomUser();
 
-    return this.prismaService.user.create({ data: randomUser });
+    return this.prismaService.users.create({ data: randomUser });
   }
 
   async createUser(user: UserModel): Promise<UserEntity> {
-    return this.prismaService.user.create({ data: user });
+    return this.prismaService.users.create({ data: user });
   }
 }
