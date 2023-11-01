28affe0560c8b3e9de9fccdcc2a8789930ee30aa
hde-oliv <henriquerocha@tutanota.com>
2023-11-01 20:03:46 -0300

    feat: add otp info on /me return

--- backend/src/chat/dto/return-me-dto.ts
+++ backend/src/chat/dto/return-me-dto.ts
@@ -4,6 +4,8 @@ export const returnMeSchema = z.object({
   nickname: z.string(),
   avatar: z.string(),
   intra_login: z.string(),
+  otp_enabled: z.boolean(),
+  otp_verified: z.boolean(),
 });
 
 export type returnMeDto = z.infer<typeof returnMeSchema>;
