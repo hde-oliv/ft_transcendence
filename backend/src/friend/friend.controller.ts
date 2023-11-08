b925e91b66f1f37dd85d65c9886dbf042c192931
hde-oliv <henriquerocha@tutanota.com>
2023-11-08 20:21:27 -0300

    feat: add friend endpoint

---
+++ backend/src/friend/friend.controller.ts
@@ -0,0 +1,33 @@
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt.guard';
+import { FriendService } from './friend.service';
+import { CreateFriendshipDto } from './dto/create-friendship-dto';
+
+@Controller('friend')
+export class FriendController {
+  constructor(private friendService: FriendService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  async getAllFriendships() {
+    return await this.friendService.getAllFriendships();
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get(':/id')
+  async getFriendship(@Param() param: { id: string }) {
+    return await this.friendService.getFriendshipById(param.id);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('/user/:id')
+  async getFriendshipsByUser(@Param() param: { id: string }) {
+    return await this.friendService.getFriendshipsByUser(param.id);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  async createFriendship(@Body() new_friendship: CreateFriendshipDto) {
+    return await this.friendService.createFriendship(new_friendship);
+  }
+}
