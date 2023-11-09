1b9d150e553baef130291cf470a92835b125aea0
Henrique de Campos Duller <henriqueduller@hotmail.com>
2023-11-09 12:03:31 -0300

    added providers to friendModule in order to make it work

--- backend/src/friend/friend.module.ts
+++ backend/src/friend/friend.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { FriendController } from './friend.controller';
 import { FriendService } from './friend.service';
+import { FriendRepository } from './friend.repository';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
+  imports: [],
   controllers: [FriendController],
-  providers: [FriendService],
+  providers: [FriendService, FriendRepository, PrismaService],
 })
-export class FriendModule {}
+export class FriendModule { }
