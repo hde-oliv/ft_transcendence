bf2ef32f8daa031bc3588d7aee325932ddaf4178
hde-oliv <henriquerocha@tutanota.com>
2023-11-14 18:29:17 -0300

    fix: friends

--- backend/src/friend/friend.service.ts
+++ backend/src/friend/friend.service.ts
@@ -7,18 +7,18 @@ export class FriendService {
   constructor(private friendRepository: FriendRepository) {}
 
   async getAllFriendships() {
-    return await this.getAllFriendships();
+    return await this.friendRepository.getAllFriendships();
   }
 
   async getFriendshipById(id: string) {
-    return await this.getFriendshipById(id);
+    return await this.friendRepository.getFriendshipById(id);
   }
 
   async getFriendshipsByUser(user_id: string) {
-    return await this.getFriendshipsByUser(user_id);
+    return await this.friendRepository.getFriendshipsByUser(user_id);
   }
 
   async createFriendship(new_friendship: CreateFriendshipDto) {
-    return await this.createFriendship(new_friendship);
+    return await this.friendRepository.createFriendship(new_friendship);
   }
 }
