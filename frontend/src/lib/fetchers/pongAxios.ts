2e06881a854318c1028d0cb25b6f77ec9ce408d7
hde-oliv <henriquerocha@tutanota.com>
2023-11-16 16:24:39 -0300

    refactor: frontend uses env var for backend url

--- frontend/src/lib/fetchers/pongAxios.ts
+++ frontend/src/lib/fetchers/pongAxios.ts
@@ -1,30 +1,30 @@
 import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 import { getToken } from "../TokenMagagment";
 
-
 // export const baseUrl = "https://transcendence.ngrok.io/api";
-export const baseUrl = "http://localhost:3000";
+export const baseUrl = process.env.NEXT_PUBLIC_BACKEND_URL ?? "";
 
-export const wsBaseUrl = baseUrl.replace('/api', '');
+export const wsBaseUrl = baseUrl.replace("/api", "");
 
 /**
  * @description
  * @returns Instace of Axios, with baseUrl to application
  */
 export function pongAxios(token?: string): AxiosInstance {
-	let localToken = token;
-	if (!token) {
-		localToken = getToken();
-	} //TODO, all calls to pongAxios dont need to provide a token anymore, also, this function will throw if no token is available
-	const config: AxiosRequestConfig = {
-		baseURL: baseUrl,
-		headers: {
-			Accept: "Application/json",
-			"Content-Type": "application/json",
-			"Access-Control-Allow-Origin": "*",
-		},
-	};
+  let localToken = token;
+  if (!token) {
+    localToken = getToken();
+  } //TODO, all calls to pongAxios dont need to provide a token anymore, also, this function will throw if no token is available
+  const config: AxiosRequestConfig = {
+    baseURL: baseUrl,
+    headers: {
+      Accept: "Application/json",
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+  };
 
-	if (localToken && config.headers) config.headers.Authorization = `Bearer ${localToken}`;
-	return axios.create(config);
+  if (localToken && config.headers)
+    config.headers.Authorization = `Bearer ${localToken}`;
+  return axios.create(config);
 }
