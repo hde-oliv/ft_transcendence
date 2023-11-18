140210926d74b73c5a5f323ed5001657d06360f8
hde-oliv <henriquerocha@tutanota.com>
2023-11-18 15:43:12 -0300

    chore: select random images for Faker

--- faker/main.py
+++ faker/main.py
@@ -1,18 +1,20 @@
 from faker import Faker
 import requests
 from faker.providers import person
+import random
 
 f = Faker()
 f.add_provider(person)
 
 intra = f.first_name_male()
 
+avatar = random.choice(["https://i.imgur.com/YrZrRz0.jpg", "https://i.imgur.com/fMpvKk4.jpg"])
 
 user = {
     "id": intra,
     "nickname": f.first_name_male(),
     "intra_login": intra,
-    "avatar": "https://s4.anilist.co/file/anilistcdn/character/large/b210152-ZrFbuMmlgs21.jpg",
+    "avatar": avatar,
 }
 
 url = "http://localhost:3000/user"
