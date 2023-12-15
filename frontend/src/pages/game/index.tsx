022877e6ce18c6ac7102467266e58107fe2c69e7
Henrique de Campos Duller <henriqueduller@hotmail.com>
2023-12-15 13:55:14 -0300

    minor fix to game page, added pageLoyout, removed navbar

--- frontend/src/pages/game/index.tsx
+++ frontend/src/pages/game/index.tsx
@@ -1,22 +1,28 @@
 'use client'
 import PingPongTable from "@/components/game/PingPongTable";
 import PongNavBar from "@/components/nav/PongNavBar";
+import PageLayout from "@/components/pageLayout/PageLayout";
 import { Box } from "@chakra-ui/react";
+import { ReactElement } from "react";
 
-export default function Game() {
-	return (
-    <>
-    <PongNavBar nickname={""} avatar={""} intra_login={""} otp_enabled={false} />
-		<Box className="App"
-			display={'flex'}
-			justifyContent={'center'}
-			alignItems={'center'}
-			h='100vh'
-			backgroundColor={'#282c34'}
-			border='1px'
-			borderColor='blue'
-		>
-			<PingPongTable />
-		</Box></>
-	);
+function Game() {
+  return (
+    <Box className="App"
+      display={'flex'}
+      justifyContent={'center'}
+      alignItems={'center'}
+      h='100vh'
+      backgroundColor={'#282c34'}
+      border='1px'
+      borderColor='blue'
+    >
+      <PingPongTable />
+    </Box>
+  );
 }
+
+Game.getLayout = function getLayoutPage(page: ReactElement) {
+  return <PageLayout>{page}</PageLayout>;
+};
+
+export default Game;
